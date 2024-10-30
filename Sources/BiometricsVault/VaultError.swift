8daf64ebd9ecc91bffba385cd6f7dca7a7080b25
Aris Koxaras <aris@koxaras.com>
2024-10-30 22:22:32 +0200

    Removed unused code

--- Sources/BiometricsVault/VaultError.swift
+++ Sources/BiometricsVault/VaultError.swift
@@ -10,16 +10,6 @@ import Foundation
 public enum VaultError: Error, LocalizedError {
     case notAvailable
 
-    case notEnabled
-
-    case notSecuredWithBiometrics
-    case notLockedWithBiometrics
-    case alreadyLockedWithBiometrics
-    case lockedWithBiometricsUseReset
-    case lockedWithBiometricsCannotEnable
-    case alreadyUsesKeychain
-    case alreadySecuredByKeychainUseSecureFunction
-    case alreadySecuredWithBiometrics
     case retrievalFailure
     case storingFailure
 
@@ -27,24 +17,6 @@ public enum VaultError: Error, LocalizedError {
         switch self {
         case .notAvailable:
             return NSLocalizedString("biometrics_not_available", comment: "")
-        case .notEnabled:
-            return NSLocalizedString("biometrics_not_enabled", comment: "")
-        case .notSecuredWithBiometrics:
-            return NSLocalizedString("biometrics_not_secured", comment: "")
-        case .notLockedWithBiometrics:
-            return NSLocalizedString("biometrics_not_locked", comment: "")
-        case .lockedWithBiometricsUseReset:
-            return NSLocalizedString("biometrics_locked_use_reset", comment: "")
-        case .lockedWithBiometricsCannotEnable:
-            return NSLocalizedString("biometrics_cannot_enable_already_locked", comment: "")
-        case .alreadyUsesKeychain:
-            return NSLocalizedString("biometrics_keychain_active", comment: "")
-        case .alreadySecuredByKeychainUseSecureFunction:
-            return NSLocalizedString("biometrics_keychain_active_use_secure", comment: "")
-        case .alreadySecuredWithBiometrics:
-            return NSLocalizedString("biometrics_not_ready_or_enabled", comment: "")
-        case .alreadyLockedWithBiometrics:
-            return NSLocalizedString("biometrics_already_locked", comment: "")
         case .retrievalFailure:
             return NSLocalizedString("biometrics_retrieval_failure", comment: "")
         case .storingFailure:
