570f9125a49a06aae12660764f4b96689fb6a360
Aris Koxaras <aris@koxaras.com>
2024-10-30 15:15:07 +0200

    Initial Commit

---
+++ BiometricsPlayground/BiometricsPlaygroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  BiometricsPlaygroundApp.swift
+//  BiometricsPlayground
+//
+//  Created by Aris Koxaras on 30/10/24.
+//
+
+import SwiftUI
+
+@main
+struct BiometricsPlaygroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
